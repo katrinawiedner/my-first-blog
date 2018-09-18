fdc619cec64d9aa88e00faa7338e7acac02c7499
katrinawiedner <office@katrinawiedner.net>
2018-09-18 15:20:31 +0200

    My Dango Girls app, first commit

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/katrinawiedner/Dropbox/_ProjectCurrent/200-DjangoCourse/virtual/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
